f61662ca1361a00b38d4cad465472d7cd1f95f8b
vijayphani <vijayphani@hotmail.com>
2025-07-25 09:14:52 -0500

    Took care of publishing events.

--- src/core/Cpa.Fas.ProductMs.Application/Common/Interfaces/IUnitOfWork.cs
+++ src/core/Cpa.Fas.ProductMs.Application/Common/Interfaces/IUnitOfWork.cs
@@ -1,6 +1,9 @@
-﻿namespace Cpa.Fas.ProductMs.Application.Common.Interfaces;
+﻿using Cpa.Fas.ProductMs.Domain.Common;
+
+namespace Cpa.Fas.ProductMs.Application.Common.Interfaces;
 
 public interface IUnitOfWork
 {
+    void AddEntityWithEvents(BaseEntity entity);
     Task<int> CommitAsync(CancellationToken cancellationToken = default);
 }
